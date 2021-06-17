65ffb06523e8bd2bcba76ac63e4511d747f72524
xuae <xuae.git@foxmail.com>
2021-06-17 16:22:11 +0800

    🔥 删除项目初始化的组件: HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,18 +6,11 @@
       <router-link to="/about">About</router-link>
     </div>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
